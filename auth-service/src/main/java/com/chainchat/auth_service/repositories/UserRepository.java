82518a8cc4c43466409a4a9152a0b8c7091c87a4
Rayane212 <rayanehadi41@gmail.com>
2024-10-23 00:25:00 +0200

    Begin to config spring security

--- auth-service/src/main/java/com/chainchat/auth_service/repositories/UserRepository.java
+++ auth-service/src/main/java/com/chainchat/auth_service/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.chainchat.auth_service.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,9 +9,9 @@ import com.chainchat.auth_service.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    User findByEmail(String email);
-    User findByUsernameOrEmail(String username, String email);
+    Optional<User>  findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Optional<User>  findByUsernameOrEmail(String username, String email);
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
    
