3b808d14544de56acf6a370ddbcb257b04c36db2
Rayane212 <rayanehadi41@gmail.com>
2024-11-21 14:31:03 +0100

    Implement User module with service and model; add unit tests for UserService

---
+++ messaging-service/src/user/user.model.ts
@@ -0,0 +1,10 @@
+// import { Prisma } from "@prisma/client";
+
+// export class User implements Prisma.UserCreateInput{
+//     id?: string;
+//     email: string;
+//     username: string;
+//     messages?: Prisma.MessageCreateNestedManyWithoutSenderInput;
+//     receivedMessages?: Prisma.MessageCreateNestedManyWithoutRecipientInput;
+    
+// }
