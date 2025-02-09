2e51849c5df73b8e29144a2d5524be5178ee3490
Rayane212 <rayanehadi41@gmail.com>
2025-02-09 22:59:17 +0100

    Add Dockerfile for auth service, update gRPC configuration, and switch to bcryptjs

--- auth-service/src/auth/auth.module.ts
+++ auth-service/src/auth/auth.module.ts
@@ -27,8 +27,8 @@ import { join } from 'path';
         transport: Transport.GRPC,
         options: {
           package: 'mail',
-          protoPath: join(__dirname, '../../../proto/mail.proto'),
-          url: '0.0.0.0:50052'
+          protoPath: join(__dirname, '../../proto/mail.proto'),
+          url: 'mail_service:50052'
         },
       },
     ]),
